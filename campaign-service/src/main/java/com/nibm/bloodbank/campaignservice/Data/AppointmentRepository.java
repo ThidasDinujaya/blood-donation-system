368e1a33b0460ab3c39292f815a2d47af047fdf1
Steph Fedo <fedosteph2@gmail.com>
2026-07-10 23:36:10 +0530

    Creating appoinment and campaign repositories

---
+++ campaign-service/src/main/java/com/nibm/bloodbank/campaignservice/Data/AppointmentRepository.java
@@ -0,0 +1,21 @@
+package com.nibm.bloodbank.campaignservice.Data;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+    List<Appointment> findByCampaignId(int campaignId);
+
+    List<Appointment> findByDonorId(int donorId);
+
+    Appointment findByCampaignIdAndAppointmentDateAndTimeSlot(
+            int campaignId,
+            LocalDate appointmentDate,
+            String timeSlot
+    );
+
+    List<Appointment> findByStatus(String status);
+}
