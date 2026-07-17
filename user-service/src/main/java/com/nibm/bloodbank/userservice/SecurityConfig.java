dea58a4dedbbe2442bbdd4cef0289c507b4321c2
DevVinu <devvinudanadissa10@gmail.com>
2026-07-17 15:44:24 +0530

    upload

--- user-service/src/main/java/com/nibm/bloodbank/userservice/SecurityConfig.java
+++ user-service/src/main/java/com/nibm/bloodbank/userservice/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,7 +17,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 
     @Bean
